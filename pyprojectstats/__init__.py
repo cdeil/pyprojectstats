185960fc5ae38bf119be4e68f7322f8372d5de19
Christoph Deil <Deil.Christoph@gmail.com>
2019-05-14 11:52:07 +0200

    update

---
+++ pyprojectstats/__init__.py
@@ -0,0 +1,3 @@
+"""Quick and simple Python project statistics"""
+
+__version__ = "0.1"
---
+++ pyprojectstats/__main__.py
@@ -0,0 +1,12 @@
+"""Command line interface"""
+import click
+
+
+@click.command()
+def cli():
+    """Quick and simple Python project statistics"""
+    click.echo("Coming soon ...")
+
+
+if __name__ == '__main__':
+    cli()
